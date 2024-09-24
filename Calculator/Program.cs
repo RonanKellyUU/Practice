9930032b59ffc32c5cc2c562f334cd6af662c285
Ronan Kelly <Kelly-R95@ulster.ac.uk>
2024-09-24 15:52:03 +0100

    Calculator Created

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("type in the first number followed by the enter key");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+
+
+Console.WriteLine("type in the second number followed by the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} give the anser {2}", firstNumber, secondNumber, result);
